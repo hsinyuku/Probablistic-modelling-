7cfb0f61249fa079f129b995f1098d94b4376fea
InazeaAnazasi <inazea@posteo.org>
2020-06-21 16:10:56 +0200

    unimportant changes

--- R/03_RunModel_Spain.R
+++ R/03_RunModel_Spain.R
@@ -23,7 +23,7 @@ data_list_model16A = {list(
   K        = K,        # number of age groups
   age_dist = age_dist, # age distribution
   pop_t    = pop_t,    # total population
-  t0       = t0,        # start time in days
+  t0       = t0,       # start time in days
   # Controls -------------------------------#
   t_data    = t_data,
   tswitch   = tswitch,
