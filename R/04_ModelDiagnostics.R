2c61fd8575fbf85f299e20cf842b9b9d39098bed
cyrus <93.chan.le@gmail.com>
2020-07-01 18:10:10 +0200

    More plots

--- R/04_ModelDiagnostics.R
+++ R/04_ModelDiagnostics.R
@@ -21,7 +21,7 @@ region <- "Spain"
 # this line is an attempt at making the code execution stop when there are 
 # errors in on of the files to be sourced 
 source(paste0("R/02_PrepareModel_", region, ".R"))   # contains all other parameters 
-source("R/04_0ModelDiagnostics_Plotting_Functions.R") # for visualizations
+source("R/04b_ModelDiagnostics_Plotting.R") # for visualizations
 theme_set(theme_bw())
 
 # Load the saved posterior samples in the data. I don't want to source
@@ -31,14 +31,22 @@ samples <-  readRDS(file = paste0("Posteriors/",region, "_samples1000.Rds"))
 # ----------------------------------------------------------------------------#
 
 # Gather all paremeters in theta
-pars <- c("beta","epsilon","rho","pi","psi")
+pars <- c("beta", "epsilon","rho","pi","psi", "eta")
+
+
+# Function to save plots
+save_gg <- function(plot, name, region){
+  ggsave(paste0("Figures/", region, " - ", name, ".png"), units = "in", width = 10, height = 6)
+}
+
 
 
 
 # Plot the parameter posteriors and overlaying the chains to check for
 # consistence
-stan_dens(samples, pars = pars, separate_chains = T)
-
+stan_dens(samples, pars = pars, separate_chains = T) +
+  labs(title = paste0("Posterior Density Plots (",region,")"), subtitle = ("For 4 separating chains")) %>% 
+  save_gg(plot = ., name = "Den", region)
 
 print(samples, pars = pars)
 
@@ -47,17 +55,34 @@ plot_incidence_cases(samples = samples,
                      data_list = data_list_model, # sourced from 02_
                      start_date = day_data,
                      end_date = day_max,
-                     region = "spain")
+                     region = region) %>% 
+  save_gg(plot = ., name = "Incidence Cases Timeline", region)
 
 
-plot_agedist_cases(samples = samples,
-                     data_list = data_list_model #sourced from 02_
-                     )
+plot_incidence_deaths(samples = samples,
+                     data_list = data_list_model, # sourced from 02_
+                     start_date = day_data,
+                     end_date = day_max) %>% 
+  save_gg(plot = ., name = "Incidence Deaths Timeline", region)
+
+
+plot_agedist_cases_perc(samples = samples,
+                     data_list = data_list_model, #sourced from 02_
+                     region = region) %>% 
+  save_gg(plot = ., name = "Age Dist Cases", region)
+
+plot_agedist_deaths_perc(samples = samples,
+                        data_list = data_list_model, #sourced from 02_
+                        region = region) %>% 
+  save_gg(plot = ., name = "Age Dist Deaths", region)
+
 
 # Plot ascertainment rate among age groups
 plot_ascertainment(samples = samples,
                    data_list = data_list_model, # sourced from 02_
-                   region = "spain")
+                   region = region) %>% 
+  save_gg(plot = ., name = "Ascertainment Rate", region)
+
 
 
 
