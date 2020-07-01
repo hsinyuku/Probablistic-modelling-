a49e7b86f9d7fee93afe5ce18676b2d2fecb4a3a
InazeaAnazasi <inazea@posteo.org>
2020-07-01 14:00:04 +0200

    added credible intervals to plot of forcing function

--- R/99_ForcingFunction.R
+++ R/99_ForcingFunction.R
@@ -8,8 +8,7 @@ dates <- readxl::read_xlsx("data/all_regions_time-parameters.xlsx")
 forcing <- dplyr::left_join(values, dates) %>% 
   mutate(t = as.numeric(tmax - t1),
          tswitch = as.numeric(day_quarantine - t1)) %>% 
-  filter(value == "median") %>% 
-  group_by(region) %>% nest() %>% 
+  group_by(region, value) %>% nest() %>% 
   mutate(forcing = map(data, function(x) {
     return(tibble(t = 1:x$t) %>% 
              mutate(forcing = x$eta + (1-x$eta)/(1+exp(x$xi*(t-x$tswitch-x$nu)))))
@@ -17,6 +16,7 @@ forcing <- dplyr::left_join(values, dates) %>%
   unnest(cols = forcing) %>% 
   select(region, t, forcing)
 
-ggplot(forcing, aes(x = t, y = forcing, col = region)) +
-  geom_line()
+ggplot(forcing, aes(x = t, y = forcing, col = value)) +
+  facet_wrap(facets = "region") +
+  geom_line() + theme_minimal()
 
